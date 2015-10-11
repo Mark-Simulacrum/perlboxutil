33b13a44d09048c2277938c38eb6e1513e551cd1
Mark Simulacrum <mark.simulacrum@gmail.com>
2015-10-11 12:20:49 -0600

    Initial commit

---
+++ filter-mbox.pl
@@ -0,0 +1,133 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+use MIME::Parser;
+use Date::Manip;
+
+my $startDate = parseDate(shift @ARGV);
+my $endDate = parseDate(shift @ARGV);
+
+sub parseDate {
+	my ($date) = @_;
+
+	my $originalDate = $date;
+
+	$date =~ s/[^\d]+$//;
+	$date =~ s/\s*\(\w+\)$//;
+
+	my $parsedDate = ParseDate($date);
+	die "Failed to parse date: '$originalDate'" if $parsedDate eq "" && $originalDate !~ /^\s*$/;
+
+	return $parsedDate;
+}
+
+sub isDateInRange {
+	my ($startDate, $middleDate, $endDate) = @_;
+
+	return 0 unless $startDate && $middleDate && $endDate;
+
+	return Date_Cmp($startDate, $middleDate) <= 0 && Date_Cmp($endDate, $middleDate) >= 0;
+}
+
+sub isBlankLine {
+	my ($line) = @_;
+
+	return $line =~ /^\r?\n$/;
+}
+
+sub getMimeHead {
+	my ($headers) = @_;
+
+	my $parser = new MIME::Parser;
+	$parser->output_to_core(1);
+	my $entity = $parser->parse_data($headers) or die "Failed to parse headers:\n$headers";
+	return $entity->head;
+}
+
+sub processEmail {
+	my ($fromLine, $headers, $body, $filename) = @_;
+
+	my $didEmailMatch = 0;
+
+	my $head = &getMimeHead($headers);
+	if ($head->count('Date')) {
+		my $date = $head->get('Date');
+		my $parsedDate = parseDate($date);
+
+		$didEmailMatch = isDateInRange($startDate, $parsedDate, $endDate);
+	} else {
+		my $date = $fromLine;
+
+		$date =~ s/^From\s[^\s]+//;
+
+		if ($date ne '') { # If From line contained just "From example@example.com" without a date
+			my $parsedDate = parseDate($date);
+			$didEmailMatch = isDateInRange($startDate, $parsedDate, $endDate);
+		} else {
+			if ($head->count('Subject')) {
+				my $subject = $head->get('Subject');
+				warn "$filename: Unable to parse date in email with subject: '$subject'";
+			} else {
+				warn "$filename: Unable to parse date in email with no subject. From-line: $fromLine";
+			}
+		}
+	}
+
+	if ($didEmailMatch) {
+		print $fromLine;
+		print $headers;
+
+		if ($headers !~ /^X-Was-Archived-At:/ && $filename ne "-") {
+			print "X-Was-Archived-At: $filename\n";
+		}
+
+		# The blank line after the headers isn't included for easy addition
+		# of more headers.
+		print "\n";
+
+		print $body;
+	}
+}
+
+sub processFile {
+	my ($filename) = @_;
+
+	open( my $fh => $filename ) || die "Cannot open $filename: $!";
+
+	my $fromLine = '';
+	my $headers = '';
+	my $body = '';
+
+	my $isReadingHeaders = 0;
+	my $sawBlankLine = 1;
+	while (my $line = <$fh>) {
+		if ($line =~ /^From\s/ && $sawBlankLine) {
+		 	&processEmail($fromLine, $headers, $body, $filename) unless !$fromLine;
+
+		 	$fromLine = $line;
+		 	$headers = '';
+		 	$body = '';
+
+		 	$isReadingHeaders = 1;
+		} elsif ($isReadingHeaders) {
+			if (isBlankLine $line) {
+				$isReadingHeaders = 0;
+			} else {
+				$headers .= $line;
+			}
+		} else {
+			$body .= $line;
+		}
+
+		$sawBlankLine = isBlankLine $line;
+	}
+
+	&processEmail($fromLine, $headers, $body, $filename) unless !$fromLine;
+}
+
+foreach my $argument (@ARGV) {
+	processFile($argument);
+	# print "$startDate to $endDate: $argument\n";
+}
---
+++ find-missing.pl
@@ -0,0 +1,98 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+use Digest::SHA qw ( sha512 );
+
+my %emailHashes = ();
+sub isBlankLine {
+	my ($line) = @_;
+
+	return $line =~ /^\r?\n$/;
+}
+
+sub processEmail {
+	my ($fromLine, $headers, $body, $filename) = @_;
+
+	if (!exists $emailHashes{sha512($body)}) {
+		print $fromLine;
+		print $headers;
+
+		if ($headers !~ /^X-Was-Archived-At:/ && $filename ne "-") {
+			print "X-Was-Archived-At: $filename\n";
+		}
+
+		# The blank line after the headers isn't included for easy addition
+		# of more headers.
+		print "\n";
+
+		print $body;
+	}
+}
+
+sub hashEmail {
+	my ($fromLine, $headers, $body, $filename) = @_;
+
+	$emailHashes{sha512($body)} = undef;
+}
+
+sub processFile {
+	my ($filename, $processEmail) = @_;
+
+	open( my $fh => $filename ) || die "Cannot open $filename: $!";
+
+	my $fromLine = '';
+	my $headers = '';
+	my $body = '';
+
+	my $isReadingHeaders = 0;
+	my $sawBlankLine = 1;
+	while (my $line = <$fh>) {
+		if ($line =~ /^From\s/ && $sawBlankLine) {
+		 	&{$processEmail}($fromLine, $headers, $body, $filename) unless !$fromLine;
+
+		 	$fromLine = $line;
+		 	$headers = '';
+		 	$body = '';
+
+		 	$isReadingHeaders = 1;
+		} elsif ($isReadingHeaders) {
+			if (isBlankLine $line) {
+				$isReadingHeaders = 0;
+			} else {
+				$headers .= $line;
+			}
+		} else {
+			$body .= $line;
+		}
+
+		$sawBlankLine = isBlankLine $line;
+	}
+
+	&{$processEmail}($fromLine, $headers, $body, $filename) unless !$fromLine;
+}
+
+my @toBeHashed = ();
+my @toBeChecked = ();
+
+my $hasSeenSplitter = 0;
+foreach my $argument (@ARGV) {
+	if ($argument eq '--in') {
+		$hasSeenSplitter = 1;
+	} else {
+		if (!$hasSeenSplitter) {
+			push @toBeHashed, $argument;
+		} else {
+			push @toBeChecked, $argument;
+		}
+	}
+}
+
+foreach my $filename (@toBeHashed) {
+	&processFile($filename, \&hashEmail);
+}
+
+foreach my $filename (@toBeChecked) {
+	&processFile($filename, \&processEmail);
+}
